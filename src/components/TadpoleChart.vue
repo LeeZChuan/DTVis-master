d9d56c18cd8f1380271e78a02c8d7768e4960ebc
LeeZChuan <cdutlzc@gmail.com>
2020-03-12 14:12:52 +0800

    Update

---
+++ src/components/TadpoleChart.vue
@@ -0,0 +1,132 @@
+<template>
+  <div id="app">
+    <div id="myChart" style="width: 600px;height:400px;"></div>
+  </div>
+</template>
+
+<!--蝌蚪图-->
+<script>
+var echarts = require("echarts");
+// 引入 ECharts 主模块
+var echarts = require("echarts/lib/echarts");
+// 引入柱状图
+require("echarts/lib/chart/bar");
+// 引入提示框和标题组件
+require("echarts/lib/component/tooltip");
+require("echarts/lib/component/title");
+
+export default {
+  name: "TadpoleChart",
+  mounted() {
+    /*ECharts图表*/
+    // function kedoutu(f, g) {}
+    var tempbusLinesk = new Array();
+    var tempbusLinesk2 = new Array();
+    var myChart = echarts.init(document.getElementById("TadpoleCharts"));
+    var didiData = new Array();
+    $.ajax({
+      // 读取json文件到didiData
+      url: "../static/data/TadpoleChart/2017-05-13/0小时.json",
+      async: false,
+      success: function(data) {
+        didiData = data;
+      }
+    });
+
+    var hStep = 300 / (didiData.length - 1); //路径颜色
+    busLinesk = [].concat.apply(
+      [],
+      didiData.map(function(busLine, idx) {
+        var points = []; //每条轨迹的数据
+        for (var i = 0; i < busLine[0].length; i += 2) {
+          var pt = [busLine[0][i], busLine[0][i + 1]]; //初始化pt坐标
+          points.push([pt[0], pt[1]]); //存入points
+        }
+        return {
+          //往buslines返回数据
+          coords: points,
+          lineStyle: {
+            normal: {
+              color: echarts.color.modifyHSL(
+                "rgb(255,122,0)",
+                Math.round(hStep * idx)
+              ) //
+            }
+          }
+        };
+      })
+    );
+    tempbusLinesk = busLinesk;
+
+    var busLinesk2 = [].concat.apply(
+      [],
+      didiData.map(function(busLine, idx) {
+        //用于蝌蚪图画线，数据处理待优化
+        var points = [];
+        for (var i = 0; i < busLine[0].length; i += 2) {
+          var pt = [busLine[0][i], busLine[0][i + 1]];
+          points.push([pt[0], pt[1]]); //存入points
+        }
+        return {
+          coords: points //往buslines返回数据
+        };
+      })
+    );
+    tempbusLinesk2 = busLinesk2;
+    myChartk.setOption(
+      (option = {
+        //标题
+        title: {
+          text: "海口市车流量蝌蚪图",
+          left: "center",
+          top: 20,
+          textStyle: {
+            color: "#cdddf7",
+            fontSize: 20
+          },
+          subtextStyle: {
+            color: "white",
+            fontSize: 16
+          }
+        },
+        // 加载 amap 组件
+        amap: {
+          // 高德地图支持的初始化地图配置
+          center: [110.32835483551025, 20.01996791722277], // 高德地图初始中心经纬度
+          zoom: 13, // 高德地图初始缩放级别
+          resizeEnable: true, // 是否开启resize
+          mapStyle: "amap://styles/grey", // 自定义地图样式主题
+          echartsLayerZIndex: 2019, // 高德地图自定义EchartsLayer的zIndex，默认2000
+          // 说明：如果想要添加卫星、路网等图层
+          // 暂时先不要使用layers配置，因为存在Bug
+          // 建议使用amap.add的方式，使用方式参见最下方代码
+          viewMode: "3D", //开启3D视图,默认为关闭
+          buildingAnimation: true //楼块出现是否带动画
+        },
+        series: [
+          {
+            type: "lines",
+            coordinateSystem: "amap",
+            polyline: true,
+            data: busLinesk,
+            lineStyle: {
+              normal: {
+                width: 0
+              }
+            },
+            effect: {
+              constantSpeed: 20,
+              show: true,
+              trailLength: 0.1,
+              symbolSize: 1.5
+            },
+            zlevel: 1
+          }
+        ]
+      })
+    );
+  }
+
+  
+};
+</script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,8 +1,9 @@
 <script>
 import exp from '../components/exp.vue'
+import TadpoleChart from '../components/TadpoleChart/TadpoleChineart.vue'//蝌蚪图
+import LineChart from '../components/LineChart/LineChart.vue'//蝌蚪图
 // 网页界面设计
 // import dashboardVue from '../view/dashboard.vue';
-// import Tadpole from '../components/Tadpole/Tadpole chart'//蝌蚪图
 // import Forecast from'../components/Forecast/Forecast'//预测界面
 // import HeatMap from'./components/3Dhot/3Dhot chart.vue'//热力图界面
 // import multiputeMap from'./components/multiputeMap.vue'//用于展示海口市地区订单情况散点雷达图
@@ -11,10 +12,15 @@ export default {
   // name:'dashboard',
   components: {
     exp,
+    TadpoleChart
   },
 };
 </script>
 
 <template>
+<div>
     <exp></exp>
+    <TadpoleChart></TadpoleChart>
+    <LineChart></LineChart>
+</div>
 </template>
