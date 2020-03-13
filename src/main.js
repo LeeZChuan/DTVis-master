51428eeee66b1fd173d6cc0b17afdeefd467da2a
LeeZChuan <cdutlzc@gmail.com>
2020-03-13 20:46:59 +0800

    update png

--- src/main.js
+++ src/main.js
@@ -7,16 +7,17 @@ import axios from 'axios';
 import echarts from 'echarts';
 import ElementUI from 'element-ui';//引入element-ui
 import 'element-ui/lib/theme-chalk/index.css';
-// import iView from 'iview';
 import 'echarts-gl';
-
 
 // import 'iview/dist/styles/iview.css';
 // import './assets/dark';
+// import iView from 'iview';
 
 Vue.config.productionTip = false;
 Vue.prototype.$axios = axios;
 Vue.prototype.$bus = new Vue();
+require('./static/js/common.js')
+require('./static/js/jquery-1.8.3.min.js')
 Vue.use(ElementUI);
 Vue.use(echarts);
 // Vue.use(iView);
---
+++ src/views/Dashboard1.vue
@@ -0,0 +1,36 @@
+<script>
+//该文件为网页效果的备份版本
+import exp from "../components/exp.vue"; //样例图
+import TadpoleChart from "../components/TadpoleChart.vue"; //蝌蚪图
+import LineChart from "../components/LineChart.vue"; //出行距离与出行次数折线图
+// import ForecastChart from "../components/ForecastChart"; //预测界面
+// import HeatMapChart from "./components/3DhotChart.vue"; //热力图界面
+// import multiputeMap from "./components/multiputeMap.vue"; //用于展示海口市地区订单情况散点雷达图
+// 网页界面设计
+// import dashboardVue from '../view/dashboard.vue';
+// export 作用
+import "../assets/css/style.css";
+export default {
+  components: {
+    exp,
+    TadpoleChart,
+    LineChart
+    // ForecastChart,
+    // HeatMapChart,
+    // multiputeMap
+  }
+};
+</script>
+
+<template>
+  <div>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    <exp></exp>
+    <TadpoleChart></TadpoleChart>
+    <LineChart></LineChart>
+    </el-container>
+
+  </div>
+</template>
