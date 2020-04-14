a15f748a1b4db86baf0285d5f56c35bd1ff44108
LeeZChuan <cdutlzc@gmail.com>
2020-04-14 16:43:31 +0800

    更新

--- src/components/hexiantu.vue
+++ src/components/hexiantu.vue
@@ -6,15 +6,12 @@
 <script>
 import { Chart } from "highcharts-vue";
 var Highcharts = require("highcharts");
-
 // 在 Highcharts 加载之后加载功能模块
 require("highcharts/modules/exporting")(Highcharts);
-
 // import Highcharts from "highcharts/highstock";
 // import HighchartsMore from "highcharts/highcharts-more";
 // import HighchartsDrilldown from "highcharts/modules/drilldown";
 // import Highcharts3D from "highcharts/highcharts-3d";
-
 // HighchartsMore(Highcharts);
 // HighchartsDrilldown(Highcharts);
 // Highcharts3D(Highcharts);
