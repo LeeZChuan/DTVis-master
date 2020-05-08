502b03bb10c66de3a53f9270fd45da205cd8166d
Qyx-qyx <qyx1026@foxmail.com>
2020-05-08 14:32:38 +0800

    update

--- src/components/ForecastPointChart.vue
+++ src/components/ForecastPointChart.vue
@@ -14,7 +14,6 @@ export default {
   },
   mounted() {
     this.drawChart();
-    this.readata();
   },
   methods: {
     drawChart() {
@@ -30,7 +29,6 @@ export default {
         [110.3486952530797, 20.01874102105557],                                                            // 政府  
       ];
       var colors = ['#9fd0d5','#f68e57','#eed5eb','#a65bc2','#9ecfd4','#f8ba7f','#a9e2cb','#eda3b8','#eff3b8'] // 颜色(需要修改)
-      var rads = this.readata()
       var map = new AMap.Map("Chart", {
         viewMode: "3D",
         pitch: 0,//俯仰角度，默认0，[0,83]，2D地图下无效 。
@@ -43,76 +41,46 @@ export default {
         rotateEnable:true,
         pitchEnable:true
       });
-
-      // 构造矢量圆形 circles[] 9个地点
       
-      console.log(rads)
-      console.log(rads.points)
-      // var t = rads.points.forEach(item =>{
-      //   return item
-      //   console.log(t)
-      // });
-      // console.log(rads.points.value)
-      // console.log(rads.points.value[2])
-      // console.log(rads.points[2])
-      var a= new Array()
-      a = rads.points
-      console.log(a)
-      console.log(a[2])
-      console.log(geoCoordMap)
-      // console.log(rads.points.forEach(v=>{v}))
-      // console.log(parseInt(rads.points[i]))
-      // console.log(geoCoordMap[5][0])
-      var circles = new Array()
       for(var i=0;i<=8;i++){
-        //  console.log(rads.points)
+        var circles = new Array()
+        var data = "2017-05-13"
+        var timeindex = "15";
+        var name = ['Station','University','Square','Hotal','Hospital2','Market','Hospital1','Friendship','Government']
+        this.$axios.get("../../static/data/yuce/"+ name[i] +"/" + data + ".json").then(res => {
+          var temp = res.data[timeindex]
+          console.log(temp)
+          var rads = temp.proVal
+          console.log(rads)
           circles[i] = new AMap.Circle({
-          // 数据读取
-          center: new AMap.LngLat(geoCoordMap[i][0],geoCoordMap[i][1]), // 圆心位置
-          // 半径 数据读取 
-          radius: a[i],//500,  
-          strokeColor: colors[i],  //线颜色
-          strokeOpacity: 0.5,  //线透明度
-          strokeWeight: 1,  //线粗细度
-          fillColor: colors[i],  //填充颜色
-          fillOpacity: 0.5, //填充透明度
-          // content: "<div class='info'>此处有问题，需要修改</div>"
+            // 数据读取
+            center: new AMap.LngLat(geoCoordMap[i][0],geoCoordMap[i][1]), // 圆心位置
+            // 半径 数据读取 
+            radius: 500,//parseInt(rads),//500,  
+            strokeColor: colors[i],  //线颜色
+            strokeOpacity: 0.5,  //线透明度
+            strokeWeight: 1,  //线粗细度
+            fillColor: colors[i],  //填充颜色
+            fillOpacity: 0.5, //填充透明度
+          });  
+          console.log(circles)
+          // console.log(parseFloat(rads))
+          // 添加覆盖物
+          map.add(circles);
         });
+      // console.log(circles)
       } 
-      // 添加覆盖物
-      map.add(circles);
+      
       var Opts = {
       };
       //初始化heatmap对象
       var thismap = new AMap.Map(map, Opts);
       //设置数据集
       thismap.setDataSet({
-        data: rads.points,//thisData,   
+        data: points,//thisData,   
         resizeEnable: true,
         max: 20
       });
-    },
-    readata(){
-      // 读取的时间
-      var data = "2017-05-13"
-      var timeindex = "15";
-      var name = ['Station','University','Square','Hotal','Hospital2','Market','Hospital1','Friendship','Government']
-      var points = new Array()
-      // console.log("tempdata");
-      for(var j=0;j<=8;j++){
-        this.$axios.get("../../static/data/yuce/"+ name[j] +"/" + data + ".json").then(res => {
-          // 从9个文件中读取数据：对应日期、地点和时间的 {} 对象，将其存入points数组
-          var tempdata = res.data
-          // console.log(tempdata);
-          // points.push(tempdata[timeindex]);
-          var radius = tempdata[timeindex].proVal
-          points.push(radius);
-          // points[j] = radius
-        });
-      }
-      return{
-        points
-      }
     }
   }
 }
